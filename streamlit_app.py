f1993c2b3c267e6a9f1fab2423c130ef663c7184
James Price <jwp@jameswprice.io>
2022-06-28 10:32:15 -0400

    Update streamlit_app.py
    added Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My New Healthy Diet')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Rolled Oats & egg Whites')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
