c805c29f1c9b0c0e4caee85b8e4f95555f70beb4
James Price <jwp@jameswprice.io>
2022-06-28 10:35:03 -0400

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My New Healthy Diet')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Rolled Oats & egg Whites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
